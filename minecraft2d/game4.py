5e1d7e3640c8eba99e6e9e72b240668c87ec26e3
yvan <yvanscher@gmail.com>
2017-10-19 16:46:49 -0400

    added games 3 edits and game4

--- minecraft2d/game4.py
+++ minecraft2d/game4.py
@@ -4,18 +4,34 @@ import pygame
 
 from pygame.locals import *
 
-# declare resources
 DIRT, GRASS, WATER, COAL, CLOUD, WOOD = 0, 1, 2, 3, 4, 5
-# declare valuable resources
 FIRE, SAND, GLASS, ROCK, STONE, BRICK, DIAMOND = 6, 7, 8, 9 , 10, 11, 12
-
+# declare resources for inventory interface
+resources = [DIRT, GRASS, WATER, COAL, WOOD, FIRE, SAND, GLASS, ROCK, STONE, BRICK, DIAMOND]
+# white color for number in inventory
+WHITE = (255,255,255)
 BLACK = (0,0,0)
-
 TILESIZE = 40
 MAPWIDTH = 30
 MAPHEIGHT = 20
 
-# import an image for each of the resources
+# intialize an inventory
+# empty when starting
+inventory = {
+    DIRT: 0,
+    GRASS: 0,
+    WATER: 0,
+    COAL: 0,
+    WOOD: 0,
+    FIRE: 0,
+    SAND: 0,
+    GLASS: 0,
+    ROCK: 0,
+    STONE: 0,
+    BRICK: 0,
+    DIAMOND: 0
+}
+
 textures = {
     DIRT: pygame.image.load('dirt.png'),
     GRASS: pygame.image.load('grass.png'),
@@ -35,20 +51,12 @@ textures = {
 player = pygame.image.load('char.png')
 playerPos = [0,0]
 
-# initialize the map with all dirt
 tilemap = [[DIRT for w in range(MAPWIDTH)] for h in range(MAPHEIGHT)]
-# for each row
 for row in range(MAPHEIGHT):
-    # for each column in that row
     for col in range(MAPWIDTH):
-        # generate a random number
         rn = random.randint(0,15)
-        # it the random number is 0
-        # fill the tilef with COAL
         if rn == 0:
             tile = COAL
-        # if that number is 1 or 2
-        # fill the tile with water
         elif rn in [1, 2]:
             tile = WATER
         elif rn in [3,4,5,6,7]:
@@ -62,7 +70,10 @@ for row in range(MAPHEIGHT):
         tilemap[row][col] = tile
 
 pygame.init()
-DISPLAYSURF = pygame.display.set_mode((MAPWIDTH*TILESIZE,MAPHEIGHT*TILESIZE))
+# add 50 pixels to the height for the inventory
+DISPLAYSURF = pygame.display.set_mode((MAPWIDTH*TILESIZE,MAPHEIGHT*TILESIZE+50))
+# setup a font for displaying inventory numbers
+INVFONT = pygame.font.Font('freesansbold.ttf', 18)
 
 while True:
     DISPLAYSURF.fill(BLACK)
@@ -79,14 +90,28 @@ while True:
                 playerPos[1] += 1
             if event.key == K_UP and playerPos[1] > 0:
                 playerPos[1] -= 1
+            # if we press the space key
+            if event.key == K_SPACE:
+                # store the current tile
+                currentTile = tilemap[playerPos[1]][playerPos[0]]
+                # add one of that resource to the inventory
+                inventory[currentTile] += 1
+                # replace the tile on the ground with dirt
+                tilemap[playerPos[1]][playerPos[0]] = DIRT
 
-    # render the tilemap we generated above
-    # make sure to render this before the character
-    # if not the character may not appear (hell be under the map)
-    # the rednering order matters
+    # render tilemap
     for row in range(MAPHEIGHT):
         for column in range(MAPWIDTH):
             DISPLAYSURF.blit(textures[tilemap[row][column]], (column*TILESIZE,row*TILESIZE))
+
+    # render inventory
+    placePosition = 10
+    for item in resources:
+        DISPLAYSURF.blit(textures[item], (placePosition, MAPHEIGHT*TILESIZE+20))
+        placePosition += 30
+        textObj = INVFONT.render(str(inventory[item]), True, WHITE, BLACK)
+        DISPLAYSURF.blit(textObj,(placePosition,MAPHEIGHT*TILESIZE+20))
+        placePosition += 50
 
     DISPLAYSURF.blit(player,(playerPos[0]*TILESIZE, playerPos[1]*TILESIZE))
     pygame.display.update()
