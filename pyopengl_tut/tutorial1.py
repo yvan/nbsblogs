959e86bec1d59db940566b6b9ac05a78d2c061c0
yvan <yvanscher@gmail.com>
2018-07-25 17:30:44 -0400

    fixed tutorial1

--- pyopengl_tut/tutorial1.py
+++ pyopengl_tut/tutorial1.py
@@ -1,26 +1,19 @@
-# http://pyopengl.sourceforge.net/context/tutorials/shader_1.html
 '''
-conda create -n opengl-py2 python=2 -y
-pip install TTFQuery PyOpenGL PyOpenGL-accelerate
-pip install pydispatcher PyVRML97 PyVRML97-accelerate simpleparse
-pip install OpenGLContext
-conda install numpy
-pip install Pillow
-'''
+http://pyopengl.sourceforge.net/context/tutorials/shader_1.html
 
-'''
-main takeaway
+you use glVertexPointer to point at your data in teh VBO. you gotta bind your
+VBO data into the gpu
 
-you user glVertexPointer to point at your data in teh VBO. you gotta bind your VBO data into the gpu
+glDrawArrays used to draw vertices. the vertex shader plugs in the points the
+fragment shader colors fragments between the points
 
-glDrawArrays used to draw vertices. the vertex shader plugs in the points the fragment shader colors fragments between the points
+vertex shader needs to return gl_Position fragment shader needs to return
+gl_FragColor
 
-vertex shader needs to return gl_Position fragment shader needs to return gl_FragColor
+gl_ModelViewProjectionMatrix just says to multiply our vertices by whatever
+numbers to get the viewpoint right given our vertices in the vbo
 
-gl_ModelViewProjectionMatrix just says to multiply our vertices by whatever nuymbers to get
-the viewpoint right given our vertices in the vbo
-
-you can only draw triangles with GL_TRIANGLES
+you can only draw vertices / triangles with GL_TRIANGLES
 '''
 
 # this is for creating a window
@@ -33,13 +26,13 @@ from OpenGL.GL import *
 # import the vertext buffer object
 from OpenGL.arrays import vbo
 
-# # opengl context just imports numpy or alternative
+# opengl context just imports numpy or alternative
 from OpenGLContext.arrays import *
 
-# get sahders
+# get shaders
 from OpenGL.GL import shaders
 
-# creates a simple vertex shader_1
+# create the class that runs the opengl context
 class TestContext(BaseContext):
     def OnInit(self):
         # create a vertex and fragment shader
@@ -81,23 +74,26 @@ class TestContext(BaseContext):
         # tell opengl to use our shader
         shaders.glUseProgram(self.shader)
         try:
-            #send data to gpu?
+            # bind data into gpu
             self.vbo.bind()
             try:
-                # tells opengl to access the vbo
-                glEnableClientState(GL_VERTEX_ARRAY);
+                # tells opengl to access vertex once
+                # we call a draw function
+                glEnableClientState(GL_VERTEX_ARRAY)
+                # point at our vbo data
                 glVertexPointerf(self.vbo)
                 # actually tell opengl to draw
-                # the stuff in the VBO
+                # the stuff in the VBO as a series
+                # of triangles
                 glDrawArrays(GL_TRIANGLES, 0, 9)
             finally:
-                # unbind the our data
-                # and unset our vertext array
-                # so other types of code can work
+                # cleanup, unbind the our data from gpu ram
+                # and tell opengl that it should not
+                # expect vertex arrays anymore
                 self.vbo.unbind()
                 glDisableClientState(GL_VERTEX_ARRAY)
         finally:
-            # unuse our shader
+            # stop using our shader
             shaders.glUseProgram(0)
 
 if __name__ == '__main__':
