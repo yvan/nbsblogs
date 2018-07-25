051c1d99bb9c3e904e055b19993b7249bb88f845
yvan <yvanscher@gmail.com>
2018-07-25 18:02:44 -0400

    tut3 cleanup

--- pyopengl_tut/tutorial3.py
+++ pyopengl_tut/tutorial3.py
@@ -1,23 +1,13 @@
-# http://pyopengl.sourceforge.net/context/tutorials/shader_1.html
 '''
-conda create -n opengl-py2 python=2 -y
-pip install TTFQuery PyOpenGL PyOpenGL-accelerate
-pip install pydispatcher PyVRML97 PyVRML97-accelerate simpleparse
-pip install OpenGLContext
-conda install numpy
-pip install Pillow
-'''
+http://pyopengl.sourceforge.net/context/tutorials/shader_3.html
 
-'''
-uniform values are kinda like global values like varying values and can be used to share info
-between vert and frag shaders but they dont change (as oppsoed to varying values which do).
-
-uniform values are the same for the entire rendering call.
+uniform values are like global values, similarly to varying values they can be
+used to share info between vert and frag shaders but they dont change
+(as opposed to varying values which do). uniform values are the same
+for the entire rendering call.
 
 fttransform takes vertexes and transforms them to have the correct positions
 instead of gl_ModelViewProjectionMatrix * gl_Vertex, its just more performant
-but does the same
-
 '''
 
 from OpenGLContext import testingcontext
@@ -30,20 +20,27 @@ from OpenGL.GL import shaders
 class TestContext(BaseContext):
     def OnInit(self):
         vertex = '''
+        //declare 2 uniform value for the fog
         uniform float end_fog;
         uniform vec4 fog_color;
+
         void main() {
             float fog;
             float fog_coord;
+
             //calculate positions
             gl_Position = ftransform();
+
             //get the ddistance along z
             fog_coord = abs(gl_Position.z);
+
             //set it to be on the scale of the fog
             fog_coord = clamp(fog_coord, 0.0, end_fog);
+
             //caluclate how much fog
             fog = (end_fog - fog_coord)/end_fog;
             fog = clamp(fog, 0.0, 1.0);
+
             //pass result to this which goes into gl_Color
             gl_FrontColor = mix(fog_color, gl_Color, fog);
         }
@@ -69,6 +66,7 @@ class TestContext(BaseContext):
                 [  2, 1, 0,  0,1,1 ],
             ], 'f')
         )
+        # get the locations in shader memory for our two uniform values
         self.UNIFORM_LOCATIONS = {
             'end_fog': glGetUniformLocation(self.shader, 'end_fog'),
             'fog_color': glGetUniformLocation(self.shader, 'fog_color')
@@ -77,11 +75,14 @@ class TestContext(BaseContext):
     def Render(self, mode=0):
         BaseContext.Render(self, mode)
         glUseProgram(self.shader)
-        #where we actually pass in values to the fog
-        glUniform1f(self.UNIFORM_LOCATIONS['end_fog'],15)
-        #switched my fog to 0000 from 1111 black to make it
-        #blend in with background
+        
+        # where we actually pass in values to the fog uniform values
+        # switched my fog to 0000 from 1111 black to make it blend in
+        # with background
+        glUniform1f(self.UNIFORM_LOCATIONS['end_fog'], 15)
         glUniform4f(self.UNIFORM_LOCATIONS['fog_color'], 0, 0, 0, 0)
+
+        # rotate the triangle and zoom in
         glRotate(45,0,1,0)
         glScale(3,3,3)
         try:
