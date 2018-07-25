f0e753fff6bcb43d6f42a7a35d8d0dd12468907f
yvan <yvanscher@gmail.com>
2018-07-25 18:42:13 -0400

    cleanup tut5

--- pyopengl_tut/tutorial5.py
+++ pyopengl_tut/tutorial5.py
@@ -1,46 +1,42 @@
-# http://pyopengl.sourceforge.net/context/tutorials/shader_4.html
 '''
-conda create -n opengl-py2 python=2 -y
-pip install TTFQuery PyOpenGL PyOpenGL-accelerate
-pip install pydispatcher PyVRML97 PyVRML97-accelerate simpleparse
-pip install OpenGLContext
-conda install numpy
-pip install Pillow
-'''
+http://pyopengl.sourceforge.net/context/tutorials/shader_5.html
 
-'''
-this tutorial is all about lighting,
-
-diffuse lighting + ambient lighting only.
+this tutorial is about lighting, diffuse lighting + ambient lighting only.
 
 this photo explains ambient + diffuse + specular very well:
 
 https://upload.wikimedia.org/wikipedia/commons/6/6b/Phong_components_version_4.png
 
-ambient lighting -- is the light an object emits if there are no other lights
-in the scene. it simulates the effect of light bouncing around theenvironment and
-isnt accounted for by any other direct light sources. this makes sense. in the real world
-light bounces off stuff  and everything has a
+ambient lighting is the light an object emits if there are no other lights
+in the scene. it simulates the effect of light bouncing around the environment
+and isnt accounted for by any other direct light sources. this makes sense.
+in the real world light bounces off stuff and you can see objects without
+direct light
 
-diffuse lighting (lambertian reflectance) is light that emits evenly in all directions
-when alight hits the object. think if of the 'matte' finish you can get on things
-or anti glare screens anti glare is actually an application of using diffuse lighting (via a material with high
-diffusion).
+diffuse lighting (lambertian reflectance) is light that emits evenly in all
+directions when alight hits the object. think if of the 'matte' finish you can
+get on things or anti glare screens anti glare is actually an application of
+using diffuse lighting (via a material with high diffusion).
 
-eye space vs model space - basically when we pass values to phong wieght calc or wtvr we have 2 forms for the models
-basically eye space is what your eye would see and model space is like if you dont multipley by the normals
-most tutorials in eye space and its easier to reason about.
+eye space vs model space - when we pass values to phong wieght calc
+we have 2 forms for the models. eye space is what your eye
+would see and model space is like if you dont mulitply by the normals most
+tutorials in eye space and its easier to reason about.
 
-phong lighting - this takes a light position and a fragment normal takes their dot product (bounded at 0) and returns
-the value
+phong lighting - this takes a light position and a fragment normal takes
+their dot product (bounded at 0) and returns the weight value to use to multiply
+by the light value, so the relfection for a particular pixel can be calculated
 
-this vertex shader returns acolor the color is determined by the global ambiehnt light the ambient light contribution of
-the point light we have (whyen we turn on our llight it increases ambient light as well).
+this vertex shader returns a color determined by the global ambient
+light. the ambient light contribution of the point light we have (when we turn
+on our light it increases ambient light as well).
 
-the diffuse light value * materials duiffuse reflectance * the diffuse wieght which is returned from the phong lighting
+the diffuse light value * materials duiffuse reflectance * the diffuse phong
+which is returned from the phong lighting
 
-normalize gets a vector in the same dirction as the original vector but the vector has a magnitude of 1.
-see: https://www.khronos.org/registry/OpenGL-Refpages/gl4/html/normalize.xhtml
+normalize gets a vector in the same dirction as the original vector but the
+vector has a magnitude of 1 see:
+https://www.khronos.org/registry/OpenGL-Refpages/gl4/html/normalize.xhtml
 
 the normal matrix: http://www.lighthouse3d.com/tutorials/glsl-12-tutorial/the-normal-matrix/
 '''
@@ -60,12 +56,16 @@ class TestContext(BaseContext):
             in vec3 light_pos,
             in vec3 frag_normal
         ){
+            // get the dot product of our normal and the light position
+            // we bound it at 0
             float n_dot_pos = max(0.0, dot(frag_normal, light_pos));
             return n_dot_pos;
         }
         '''
 
         vertex = '''
+        // define a bunch of uniform values
+        // for representing lighting constants
         uniform vec4 Global_ambient;
         uniform vec4 Light_ambient;
         uniform vec4 Light_diffuse;
@@ -75,20 +75,23 @@ class TestContext(BaseContext):
         attribute vec3 Vertex_position;
         attribute vec3 Vertex_normal;
         varying vec4 baseColor;
+
         void main(){
             gl_Position = gl_ModelViewProjectionMatrix * vec4(
                 Vertex_position, 1.0
             );
-            // gets the light into eye space, coordinates
+            // gets the light into eye space coordinates
             vec3 EC_Light_location = gl_NormalMatrix * Light_location;
+
             // calculate phong weight for vertex
-            // by using its normal (in eye space) and the
-            // eye space light position, dont totally
-            // get this, norm them both so they are len(1)
+            // by using its normal and the
+            // eye space light position
+            // norm them both so they are len(1)
             float diffuse_weight = phong_weightCalc(
                 normalize(EC_Light_location),
                 normalize(gl_NormalMatrix * Vertex_normal)
             );
+
             // get a 0-1 value for this vertex color
             // that is a combination of the global light
             // the ambient light from the light source
@@ -110,8 +113,6 @@ class TestContext(BaseContext):
         fragment = '''
         varying vec4 baseColor;
         void main() {
-            //simply return the color from the vertex
-            //shader using builtin fragment shader blending
             gl_FragColor = baseColor;
         }
         '''
@@ -142,6 +143,8 @@ class TestContext(BaseContext):
             ],'f')
         )
 
+        # get memory locations for
+        # shader uniform variables
         uniform_values = (
         'Global_ambient',
         'Light_ambient',
@@ -158,6 +161,8 @@ class TestContext(BaseContext):
                 set_attrib = uniform+'_loc'
                 setattr(self, set_attrib, location)
 
+        # get the memory locations for
+        # shader attribute variables
         attribute_values = (
         'Vertex_position',
         'Vertex_normal'
@@ -176,6 +181,9 @@ class TestContext(BaseContext):
         try:
             self.vbo.bind()
             try:
+                # use the memory locations we found earlier (now python attributes
+                # on the current class) for shader variables and put data into
+                # the shader variables
                 glUniform4f( self.Global_ambient_loc, .3,.05,.05,.1 )
                 glUniform4f( self.Light_ambient_loc, .2,.2,.2, 1.0 )
                 glUniform4f( self.Light_diffuse_loc, 1,1,1,1 )
@@ -185,6 +193,7 @@ class TestContext(BaseContext):
 
                 glEnableVertexAttribArray(self.Vertex_position_loc)
                 glEnableVertexAttribArray(self.Vertex_normal_loc)
+                # reference our vertex data and normals data
                 stride = 6 * 4
                 glVertexAttribPointer(
                     self.Vertex_position_loc,
